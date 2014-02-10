86c8dd1f7720e15c57459e5bb6343167c6639390
Shoghi Cervantes <shoghicp@gmail.com>
2014-02-10 21:40:27 +0100

    Working on join too :D

--- HideMe.php
+++ HideMe.php
@@ -4,7 +4,7 @@
 __PocketMine Plugin__
 name=HideMe
 description=Hide yourself from other players, or disguise as other entities.
-version=1.0.0
+version=1.0.1
 author=shoghicp
 class=HideMe
 apiversion=12
@@ -13,11 +13,11 @@ apiversion=12
 class HideMe implements Plugin{
 	private $api;
 	private $config;
-	private $hideUsers;
+	private $hideEntities;
 	
 	public function __construct(ServerAPI $api, $server = false){
 		$this->api = $api;
-		$this->hideUsers = array();
+		$this->hideEntities = array();
 	}
 	
 	public function init(){
@@ -29,8 +29,9 @@ class HideMe implements Plugin{
 	
 	public function dataPacketHandler(DataPacketSendEvent $event){
 		$packet = $event->getPacket();
-		if($packet instanceof AddPlayerPacket or $packet instanceof MoveEntityPacket_PosRot){
-			if(isset($this->hideUsers[$event->getPlayer()->username])){
+		
+		if(($packet instanceof AddPlayerPacket) or ($packet instanceof MoveEntityPacket_PosRot) or ($packet instanceof RemovePlayerPacket)){
+			if(isset($this->hideEntities[$packet->eid])){
 				$event->setCancelled();
 			}
 		}
@@ -47,9 +48,9 @@ class HideMe implements Plugin{
 				}
 				
 				if($player instanceof Player){
-					if(isset($this->hideUsers[$player->username])){
+					if(isset($this->hideEntities[$player->entity->eid])){
 						$output = "You are now visible.\n";
-						
+						unset($this->hideEntities[$player->entity->eid]);						
 						$pk = new AddPlayerPacket;
 						$pk->clientID = 0;
 						$pk->username = $player->username;
@@ -62,20 +63,129 @@ class HideMe implements Plugin{
 						$pk->unknown1 = 0;
 						$pk->unknown2 = 0;
 						$pk->metadata = $player->entity->getMetadata();
-						unset($this->hideUsers[$player->username]);
+						foreach($this->api->player->getAll() as $p){
+							if($p->spawned === true and $p !== $player and $p->level !== $player->level){
+								$p->dataPacket($pk);
+							}
+						}
+						
+						foreach($this->api->player->getAll($player->level) as $p){
+							if($p->spawned === true and $p !== $player){
+								$player->entity->spawn($p);
+							}
+						}
 					}else{
 						$output = "You are now hidden.\n";
 						
 						$pk = new RemovePlayerPacket;
 						$pk->clientID = 0;
 						$pk->eid = $player->entity->eid;
-						$this->hideUsers[$player->username] = true;
-					}
-					
-					foreach($this->api->player->getAll() as $p){
-						if($p->spawned === true and $p !== $player){
-							$p->dataPacket($pk);
+						foreach($this->api->player->getAll() as $p){
+							if($p->spawned === true and $p !== $player){
+								$p->dataPacket($pk);
+							}
 						}
+						$this->hideEntities[$player->entity->eid] = true;
+					}			
+				}else{
+					$output = "Usage: /$cmd [player]\n";
+				}
+				break;
+		}
+		return $output;
+	}
+	
+	public function __destruct(){
+	
+	}
+}<?php
+
+/*
+__PocketMine Plugin__
+name=HideMe
+description=Hide yourself from other players, or disguise as other entities.
+version=1.0.1
+author=shoghicp
+class=HideMe
+apiversion=12
+*/
+
+class HideMe implements Plugin{
+	private $api;
+	private $config;
+	private $hideEntities;
+	
+	public function __construct(ServerAPI $api, $server = false){
+		$this->api = $api;
+		$this->hideEntities = array();
+	}
+	
+	public function init(){
+		DataPacketSendEvent::register(array($this, "dataPacketHandler"), EventPriority::HIGHEST);
+		$this->api->console->register("hide", "[player]", array($this, "commandHandler"));
+		//TODO: handle player leave
+		console("[INFO] HideMe started");
+	}
+	
+	public function dataPacketHandler(DataPacketSendEvent $event){
+		$packet = $event->getPacket();
+		
+		if(($packet instanceof AddPlayerPacket) or ($packet instanceof MoveEntityPacket_PosRot) or ($packet instanceof RemovePlayerPacket)){
+			if(isset($this->hideEntities[$packet->eid])){
+				$event->setCancelled();
+			}
+		}
+	}
+	
+	public function commandHandler($cmd, $params, $issuer, $alias){
+		$output = "";
+		switch($cmd){
+			case "hide":
+				if($issuer instanceof Player and !isset($params[0])){
+					$player = $issuer;
+				}elseif(isset($params[0])){
+					$player = $this->api->player->get($params[0]);
+				}
+				
+				if($player instanceof Player){
+					if(isset($this->hideEntities[$player->entity->eid])){
+						$output = "You are now visible.\n";
+						unset($this->hideEntities[$player->entity->eid]);						
+						$pk = new AddPlayerPacket;
+						$pk->clientID = 0;
+						$pk->username = $player->username;
+						$pk->eid = $player->entity->eid;
+						$pk->x = -256;
+						$pk->y = 128;
+						$pk->z = -256;
+						$pk->yaw = 0;
+						$pk->pitch = 0;
+						$pk->unknown1 = 0;
+						$pk->unknown2 = 0;
+						$pk->metadata = $player->entity->getMetadata();
+						foreach($this->api->player->getAll() as $p){
+							if($p->spawned === true and $p !== $player and $p->level !== $player->level){
+								$p->dataPacket($pk);
+							}
+						}
+						
+						foreach($this->api->player->getAll($player->level) as $p){
+							if($p->spawned === true and $p !== $player){
+								$player->entity->spawn($p);
+							}
+						}
+					}else{
+						$output = "You are now hidden.\n";
+						
+						$pk = new RemovePlayerPacket;
+						$pk->clientID = 0;
+						$pk->eid = $player->entity->eid;
+						foreach($this->api->player->getAll() as $p){
+							if($p->spawned === true and $p !== $player){
+								$p->dataPacket($pk);
+							}
+						}
+						$this->hideEntities[$player->entity->eid] = true;
 					}			
 				}else{
 					$output = "Usage: /$cmd [player]\n";
